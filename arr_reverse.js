a9b6bbc7ed20f12033f0e2f5e4d57ab9cd7e63fc
Sushanta65 <sushanta9119@gmail.com>
2024-09-05 23:02:03 +0600

    javaScript

---
+++ arr_reverse.js
@@ -0,0 +1,31 @@
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+// // console.log(numbers)
+// // console.log(numbers.reverse())
+// let arr = []
+// for (let i = 0; i < numbers.length; i++){
+  
+//     arr.unshift(numbers[i])
+// }
+// console.log(arr)
+
+
+// const friends = ["Elom Mask", "Bil Gates", "Mark", "Jef Bejos"]
+
+// console.log(numbers.sort())
+// console.log(friends.sort())
+
+
+let sen = "I am hard working person"
+let sent;
+const splitedSen = sen.split(" ").reverse()
+
+// for(let i = 0; i < splitedSen.length; i++){
+//     // console.log(i)
+//     sent = splitedSen[i]
+//     console.log(sent.join(" "))
+// }
+
+// console.log(sent);
+
+console.log(splitedSen.join(' '))
---
+++ script.js
@@ -0,0 +1,23 @@
+const friends = ["Elom Mask", "Bil Gates", "Mark", "Jef Bejos"]
+
+const elonMask = {
+    name: "Mask",
+    company: "Tesla",
+    earn: "250 Billion Dollar"
+}
+
+// for (const friend of friends){
+//     console.log(friend)
+// }
+
+// for (const elon in elonMask){
+//     console.log(elon)
+// }
+
+for(let i = 0; i < friends.length; i++){
+    console.log(i)
+    console.log(friends[i])
+
+
+    
+}
