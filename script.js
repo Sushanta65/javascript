76866b39fe5d7168561c22a9461b8f1e16375a2b
Sushanta65 <sushanta9119@gmail.com>
2024-09-08 23:30:55 +0600

    Practice

--- script.js
+++ script.js
@@ -23,3 +23,41 @@
 // }
 
 //Swap Variable
+
+
+// let a = 445;
+// let b = 22333;
+// let c = 884444;
+
+// if(a > b && a > c){
+//     console.log("A is the Big")
+//     if(b > c){
+//         console.log("B Is Second")
+//     }else if(b < c){
+//         console.log("C is second")
+//     }
+// }else if(b > a && b > c){
+//     console.log("B is the Bigger")
+//     if(a > c){
+//         console.log("A is second")
+//     }else if(a < c){
+//         console.log("C is third")
+//     }
+// }else if {
+//     console.log("C is the Bigger")
+// }
+
+
+// function maxNum(num1, num2, num3){
+//     if(num1 > num2 && num1 > num3){
+//         console.log("Number 1 is begger")
+//     }else if(num2 > num1 && num2 > num3){
+//         console.log("Number 2 is the begger")
+//     }else{
+//         console.log("Number 3 is begger");
+//     }
+// }
+
+// maxNum(45454512, 345454, 465)
+
+// Easest way to find the max number using 
